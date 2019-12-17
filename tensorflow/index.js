eecbfa6a998b92e801df14b1bb4799393c9a6b89
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2019-12-17 17:39:16 +0530

    Pushing tensorflow

---
+++ tensorflow/index.js
@@ -0,0 +1,19 @@
+const tf = require("@tensorflow/tfjs-node-gpu");
+
+const model = tf.sequential();
+model.add(
+  tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+);
+model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch, log) =>
+      console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
+});
